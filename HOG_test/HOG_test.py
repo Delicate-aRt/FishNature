cf371823cf2687b421e7a46980a3d9699b55a000
Delicate_aRt <roman.grubnyk@gmail.com>
2016-11-24 13:47:10 +0200

    HOG_test. Initial commit.

---
+++ HOG_test/HOG_test.py
@@ -0,0 +1,53 @@
+
+import os
+import sys
+import glob
+
+import dlib
+from skimage import io
+import cv2
+
+
+# In this example we are going to train a face detector based on the small
+# faces dataset in the examples/faces directory.  This means you need to supply
+# the path to this faces folder as a command line argument so we will know
+# where it is.
+if len(sys.argv) != 2:
+    print(
+        "Give the path to the examples/faces directory as the argument to this "
+        "program. For example, if you are in the python_examples folder then "
+        "execute this program by running:\n"
+        "    ./train_object_detector.py ../examples/faces")
+    exit()
+work_dir = sys.argv[1]
+
+
+# Now let's use the detector as you would in a normal application.  First we
+# will load it from disk.
+detector = dlib.simple_object_detector("detector.svm")
+
+# We can look at the HOG filter we learned.  It should look like a face.  Neat!
+#win_det = dlib.image_window()
+#win_det.set_image(detector)
+
+# Now let's run the detector over the images in the faces folder and display the
+# results.
+print("Showing detections on the images in the faces folder...")
+
+for f in glob.glob(os.path.join(work_dir, "*.jpg")):
+    print("Processing file: {}".format(f))
+    img = io.imread(f)
+    dets = detector(img)
+    print("Number of faces detected: {}".format(len(dets)))
+
+    for k, d in enumerate(dets):
+        print (
+        "Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(k, d.left(), d.top(), d.right(), d.bottom()))
+        cv2.rectangle(img, (d.left(), d.top()), (d.right(), d.bottom()), (255, 0, 255), 2)
+        cv2.imwrite("./saved/" + f[-7:], img)
+
+
+
+
+
+
